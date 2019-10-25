58162b8db56d26826c9b4b0099456cf591d15f05
Bruno Campos <brcampos@everis.com>
2019-10-25 07:37:26 -0300

    structure finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,21 @@
-import { AnnounceBandComponent } from './announce-band/announce-band.component';
-import { AnnouncePlayerComponent } from './announce-player/announce-player.component';
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component'
-import { SignupComponent } from './signup/signup.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { AnnounceComponent } from './announce/announce.component';
+import { SearchComponent } from "./search/search.component";
+import { AnnounceBandComponent } from "./announce-band/announce-band.component";
+import { AnnouncePlayerComponent } from "./announce-player/announce-player.component";
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+import { LoginComponent } from "./login/login.component";
+import { SignupComponent } from "./signup/signup.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AnnounceComponent } from "./announce/announce.component";
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'announce', component: AnnounceComponent },
-  { path: 'announce-player', component: AnnouncePlayerComponent },
-  { path: 'announce-band', component: AnnounceBandComponent },
+  { path: "", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "announce", component: AnnounceComponent },
+  { path: "announce-player", component: AnnouncePlayerComponent },
+  { path: "announce-band", component: AnnounceBandComponent },
+  { path: "search", component: SearchComponent }
 ];
 
 @NgModule({
@@ -22,4 +24,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
