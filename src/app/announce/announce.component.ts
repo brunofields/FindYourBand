e27ebb270bcf2dcc1d86156755c5e095c9b4a89c
Bruno Campos <brcampos@everis.com>
2019-10-25 08:24:54 -0300

    refactoring

--- src/app/announce/announce.component.ts
+++ src/app/announce/announce.component.ts
@@ -1,31 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
-import { MenuController, NavController } from '@ionic/angular';
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
+import { MenuController, NavController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-announce',
-  templateUrl: './announce.component.html',
-  styleUrls: ['./announce.component.scss'],
+  selector: "app-announce",
+  templateUrl: "./announce.component.html",
+  styleUrls: ["./announce.component.scss"]
 })
 export class AnnounceComponent implements OnInit {
-
-
-  constructor(public menu: MenuController, public navCtrl: NavController) { }
+  constructor(public menu: MenuController, public navCtrl: NavController) {}
 
   openAnnounceMenu() {
-    this.menu.enable(true, 'announceMenu');
-    this.menu.open('announceMenu');
+    this.menu.enable(true, "announceMenu");
+    this.menu.open("announceMenu");
   }
 
-  
-  redirectAnnounce(link){
+  redirectAnnounce(link) {
     this.navCtrl.navigateForward(link);
   }
 
-    logout(){
-      firebase.auth().signOut();
-    }
+  logout() {
+    firebase.auth().signOut();
+  }
 
   ngOnInit() {}
-
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,32 +1,28 @@
-import { NavController } from '@ionic/angular';
-import { MenuController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
-
+import { NavController } from "@ionic/angular";
+import { MenuController } from "@ionic/angular";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss'],
+  selector: "app-search",
+  templateUrl: "./search.component.html",
+  styleUrls: ["./search.component.scss"]
 })
 export class SearchComponent implements OnInit {
-
-  constructor(public menu: MenuController, public navCtrl: NavController) { }
+  constructor(public menu: MenuController, public navCtrl: NavController) {}
 
   openAnnounceMenu() {
-    this.menu.enable(true, 'announceMenu');
-    this.menu.open('announceMenu');
+    this.menu.enable(true, "announceMenu");
+    this.menu.open("announceMenu");
   }
 
-  
-  redirectAnnounce(link){
+  redirectAnnounce(link) {
     this.navCtrl.navigateForward(link);
   }
 
-    logout(){
-      firebase.auth().signOut();
-    }
+  logout() {
+    firebase.auth().signOut();
+  }
 
   ngOnInit() {}
-
 }
