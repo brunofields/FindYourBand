3f555e183cab6e71ddba7dca30ffed8707ef79be
Bruno Campos <brcampos@everis.com>
2019-11-11 08:38:23 -0300

    shows page

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -1,22 +1,49 @@
-import { NavController } from '@ionic/angular';
-import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+import { NavController } from "@ionic/angular";
+import { Component, OnInit } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
-  selector: 'app-shows',
-  templateUrl: './shows.component.html',
-  styleUrls: ['./shows.component.scss'],
+  selector: "app-shows",
+  templateUrl: "./shows.component.html",
+  styleUrls: ["./shows.component.scss"]
 })
 export class ShowsComponent implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  showsList = [
+    {
+      nomeShow: "Rock in Santos",
+      estiloShow: "Rock anos 2000+",
+      descricao: "Venha conhecer nosso espaço e curtir o que há de melhor!",
+      dataShow: "20/11/2019",
+      localShow: "Avenida Ana Costa, 1300",
+    },
+    {
+      nomeShow: "Rock in Santos",
+      estiloShow: "Rock anos 2000+",
+      descricao: "Venha conhecer nosso espaço e curtir o que há de melhor!",
+      dataShow: "20/11/2019",
+      localShow: "Avenida Ana Costa, 1300",
+    },
+    {
+      nomeShow: "Rock in Santos",
+      estiloShow: "Rock anos 2000+",
+      descricao: "Venha conhecer nosso espaço e curtir o que há de melhor!",
+      dataShow: "20/11/2019",
+      localShow: "Avenida Ana Costa, 1300",
+    }
+  ];
 
+
+  constructor(private navCtrl: NavController) {
+  }
+
+  
   ngOnInit() {}
-  redirect(link){
-    this.navCtrl.navigateForward('/'+ link);
+  redirect(link) {
+    this.navCtrl.navigateForward("/" + link);
   }
   logout() {
     firebase.auth().signOut();
-    this.navCtrl.navigateBack('/');
+    this.navCtrl.navigateBack("/");
   }
 }
