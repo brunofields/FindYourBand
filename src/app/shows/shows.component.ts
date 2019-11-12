7190721123b201c93be29ab696d780467803f1f0
Bruno Campos <brcampos@everis.com>
2019-11-11 23:54:08 -0300

    updating mock

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -14,29 +14,29 @@ export class ShowsComponent implements OnInit {
       nomeShow: "Rock in Santos",
       estiloShow: "Rock anos 2000+",
       descricao: "Venha conhecer nosso espaço e curtir o que há de melhor!",
-      dataShow: "11/11/2019",
+      dataShow: "11/11/2019 01H00",
       localShow: "Ana Costa, 1300",
     },
     {
       nomeShow: "Saulebas",
       estiloShow: "Samba",
       descricao: "Boteco raiz com música ao vivo!",
-      dataShow: "13/11/2019",
+      dataShow: "13/11/2019 20H00",
       localShow: "Pinheiro Machado, 983",
     },
     {
       nomeShow: "Banda Okami",
       estiloShow: "J-Rock",
       descricao: "O show aqui quem faz são vocês!",
-      dataShow: "24/11/2019",
+      dataShow: "24/11/2019 - 18H00",
       localShow: "Conselheiro Nébias, 318",
     },
     {
       nomeShow: "Los Deucudios",
-      estiloShow: "Classica",
-      descricao: "O melhor da música classica",
-      dataShow: "18/12/2019",
-      localShow: "Robin Road, 51",
+      estiloShow: "Pop Rock",
+      descricao: "A BANDA QUE VAI ESTOURAR SEUS NEURÔNIOS",
+      dataShow: "01/01/2020 - 00H00 - SUPER SHOW DA VIRADA",
+      localShow: "Praia de Copacabana",
     }
   ];
 
